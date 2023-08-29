bff2c64a27ec0911c34fe6681b091519d8248ae4
RuthRojas <ruro122020@gmail.com>
2023-08-29 01:15:11 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 10) + 1;
+const num3 = 4;
+const num4 = 8;
+const mod = num3 % num4;
+const max = Math.max(10,12,14,16,18,20)
